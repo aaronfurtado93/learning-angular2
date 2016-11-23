3cd910871ab84783df92a901b0e7be2df79d54e0
Aaron Furtado <aaronfurtado93@hotmail.com>
2016-11-23 12:42:00 +0530

    Display Hero Details
    * Created `Hero` object
    * Added template and template to display hero details

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: "./app/templates/app/app.component.html",
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  {
+ title = "Tour of Heroes";
+ hero = {
+   id: 1,
+   name: "WindStorm"
+ }
+}
